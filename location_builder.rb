ef345827ed49454f069974aa1d094fe7a5544fe1
bizarre <b@tumblbug.com>
2015-10-20 10:45:42 +0900

    location builder 작업중

--- location_builder.rb
+++ location_builder.rb
@@ -1,12 +1,19 @@
 require 'yaml'
 
-artworks_location = []
-
+locations = {}
+locations['artist_locations'] = []
 
 
 Dir["./projects/artworks/*.yml"].each do |filename|
   artwork = YAML.load_file(filename)
   
+  artist_l = {}
+  artist_l['full_name_en'] = artwork["full_name_en"]
+  artist_l['full_name_ko'] = artwork["full_name_ko"]
+  artist_l['origin_lat'] = artwork["origin_lat"]
+  artist_l['origin_lng'] = artwork["origin_lng"]
+  artist_l['url'] = filename[1..filename.length]
+
   artwork_l = {}
   artwork_l['full_name_en'] = artwork["full_name_en"]
   artwork_l['full_name_ko'] = artwork["full_name_ko"]
@@ -14,9 +21,10 @@ Dir["./projects/artworks/*.yml"].each do |filename|
   artwork_l['origin_lng'] = artwork["origin_lng"]
   artwork_l['url'] = filename[1..filename.length]
 
-  artworks_location << artwork_l
+
+  locations['artist_locations'] << artist_l
 end
 
-File.open('./projects/locations.yml', 'w') {|f| f.write artworks_location.to_yaml } #Store
+File.open('./projects/locations.yml', 'w') {|f| f.write locations.to_yaml } #Store
 
 puts "Successfully built locations.yml"
