80884ceb2fcf224360c8778e081d880607f7e828
bizarre <b@tumblbug.com>
2015-11-02 12:06:07 +0900

    영문 지도 삽입

--- javascripts/src/models/map_manager.js
+++ javascripts/src/models/map_manager.js
@@ -5,6 +5,11 @@ WY.models.MapManager = (function(){
   function MapManager(params){
     this.el_name = params.el_name;
     this.map;
+    this.map_address = {
+      en: "https://b.tiles.mapbox.com/v4/mathpractice.ef398e06/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoibWF0aHByYWN0aWNlIiwiYSI6ImNpZ2hhN2Y2eDg1Y2t2Ym04M3p0emMyMHIifQ.1Nnnb5bqrFXHpdjw5A132A",
+      ko: 'https://a.tiles.mapbox.com/v4/eroon26.36545472/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiZXJvb24yNiIsImEiOiJjaWY3cWhsbnkweGVuczNrcnZoNHB4dGhoIn0.oFbWC28lxCKcOIDiffQZuw'
+    };
+
     this.data;
     this.markers = [];
     this.oms;
@@ -20,12 +25,8 @@ WY.models.MapManager = (function(){
     init: function(){
       this.map = L.map(this.el_name).setView([37.5558393, 126.9716173], 15);
       // WY.constants.map = this.map;
-      /*
-      
-      https://b.tiles.mapbox.com/v4/mathpractice.ef398e06/7/109/48@2x.png?access_token=pk.eyJ1IjoibWF0aHByYWN0aWNlIiwiYSI6ImNpZ2hhN2Y2eDg1Y2t2Ym04M3p0emMyMHIifQ.1Nnnb5bqrFXHpdjw5A132A
-      
-       */
-      L.tileLayer('https://a.tiles.mapbox.com/v4/eroon26.36545472/{z}/{x}/{y}@2x.png?access_token=pk.eyJ1IjoiZXJvb24yNiIsImEiOiJjaWY3cWhsbnkweGVuczNrcnZoNHB4dGhoIn0.oFbWC28lxCKcOIDiffQZuw', {
+      // 
+      L.tileLayer(this.map_address[WY.constants.locale], {
         attribution: '<a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       }).addTo(this.map);
 
